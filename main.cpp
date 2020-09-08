3973c34dcc39a5b5e8cd8ae8538aa12d054dc1b9
Youssef Amr <yamr5611@aucegypt.edu>
2020-09-08 17:46:06 +0200

    version1

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include"knapSack.h"
+int main()
+{
+int val[] = { 60, 100, 120 };
+int wt[] = { 10, 20, 30 };
+int W = 50;
+int n = sizeof(val) / sizeof(val[0]); cout << knapSack(W, wt, val, n); return 0;
+}
+
